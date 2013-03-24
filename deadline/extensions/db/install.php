3a3fe0a2cc6f88294e67b9212c0811ff50449f2f
James McLean <james.mclean@unisa.edu.au>
2013-03-25 09:40:22 +1030

    added install stub for extensions

---
+++ deadline/extensions/db/install.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the events that the deadline_deadlines plugin actions.
+ *
+ * @package   deadline_extensions
+ * @copyright 2013 University of South Australia {@link http://www.unisa.edu.au}
+ * @author    James McLean <james.mclean@unisa.edu.au>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+function xmldb_deadline_extensions_install() {
+
+    // Load all current extensions in mod_assign and convert them to the new extensions module.
+
+    // Load all current overrides in mod_quiz and convert them to the new extensions module.
+
+    return true;
+}
